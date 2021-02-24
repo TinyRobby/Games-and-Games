42a372b949d353189b2078907d2e833f235ab98a
Robby <77552033+TinyRobby@users.noreply.github.com>
2021-02-24 11:00:36 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#iclude <iostream>
+#iclude <fstream>
+
+using namespace std;
+
+int main()
+{
+     cout << "Welcome to the game hub" << endl;
+     string hubstrng;
+     bool game = true;
+     while(game == true)
+     {  
+            cout << "Game hub. options:\nhelp:help\nstart:start a game\nexit:leave" << endl;
+            cin >> hubstrng;
+            if(hubstrng == "exit")
+            {
+                break;
+            }
+            if(hubstrng == "start")
+            {
+                string hubstrngstart;
+                cout << "Welcome to"
+            }
+              
+     }
+  return 0;
+}  
